ed69537ccb0bb37d37446ea108bb8e46d78385a2
oteusdev <mateusvieira.net@outlook.com>
2026-04-08 08:43:20 -0300

    feature: Aula01 - Introdução e Comentários

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01HelloDevDojo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+// Comentário de uma linha
+
+/*
+    Comentário de multiplas linhas.
+ */
+
+/**
+ * Comentário javadoc
+ */
+
+public class Aula01HelloDevDojo {
+    public static void main(String[] args){
+        System.out.println("Hello DevDojo!");
+    }
+}
